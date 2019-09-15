e8ad1d77fe6f59e60e45e2bd94efc51b17a4b60d
Adam Driscoll <adam.r.driscoll@gmail.com>
2019-09-15 13:44:32 +0530

    add recurse

--- deploy.ps1
+++ deploy.ps1
@@ -4,5 +4,5 @@ Copy-Item ./ud-chatroom.psd1 $Staging
 Copy-Item ./ud-chatroom.psm1 $Staging
 Copy-Item ./dashboard.ps1 $Staging
 Save-Module -Name UniversalDashboard -Path $Staging -AcceptLicense -RequiredVersion 2.6 
-Copy-Item -Path "$Staging/UniversalDashboard/2.6.0/*" -Destination $Staging -Container
+Copy-Item -Path "$Staging/UniversalDashboard/2.6.0/*" -Destination $Staging -Container -Recurse
 Remove-Item -Path "$Staging/UniversalDashboard/2.6.0" -Force -Recurse 
