fa6c106fd6eab65c2970a32856b721787bae175b
Adam Driscoll <adam.r.driscoll@gmail.com>
2019-09-15 11:22:54 +0530

    Accept license

--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,3 @@
 #Copy-Item ./ud-chatroom.psd1 $Env:Build_ArtifactStagingDirectory
 #Copy-Item ./ud-chatroom.psm1 $Env:Build_ArtifactStagingDirectory
-Save-Module -Name UniversalDashboard -Path "./"
+Save-Module -Name UniversalDashboard -Path "./" -AcceptLicense
