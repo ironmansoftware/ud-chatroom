e242918d898a691deeb02f221fdeb7deb742d9e3
Adam Driscoll <adam.r.driscoll@gmail.com>
2019-09-15 13:49:44 +0530

    Use code editor.

--- dashboard.ps1
+++ dashboard.ps1
@@ -1,4 +1,5 @@
-Import-Module "$PSScriptRoot/UniversalDashboard/2.6/UniversalDashboard.psd1"
+Import-Module "$PSScriptRoot/UniversalDashboard.psd1"
+Import-Module "$PSScriptRoot/UniversalDashboard.CodeEditor/UniversalDashboard.CodeEditor.psd1"
 Import-Module "$PSScriptRoot/ud-chatroom.psd1"
 
 Start-UDChatroom -Wait
