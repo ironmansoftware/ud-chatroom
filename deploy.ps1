680a20b073767d21ffcb280b9c88c3c2d1fdda52
Adam Driscoll <adam.r.driscoll@gmail.com>
2019-09-15 11:43:34 +0530

    Recurse

--- deploy.ps1
+++ deploy.ps1
@@ -5,4 +5,4 @@ Copy-Item ./ud-chatroom.psm1 $Staging
 Copy-Item ./dashboard.ps1 $Staging
 Save-Module -Name UniversalDashboard -Path $Staging -AcceptLicense -RequiredVersion 2.6 
 Copy-Item -Path "$Staging/UniversalDashboard/2.6.0/*" -Destination $Staging -Container
-Remove-Item -Path "$Staging/UniversalDashboard/2.6.0" -Force
+Remove-Item -Path "$Staging/UniversalDashboard/2.6.0" -Force -Recurse 
