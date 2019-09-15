6ddbeaa3d3c4a86dd86430640dfd7029853b97eb
Adam Driscoll <adam.r.driscoll@gmail.com>
2019-09-15 11:41:53 +0530

    Dat 0

--- deploy.ps1
+++ deploy.ps1
@@ -4,5 +4,5 @@ Copy-Item ./ud-chatroom.psd1 $Staging
 Copy-Item ./ud-chatroom.psm1 $Staging
 Copy-Item ./dashboard.ps1 $Staging
 Save-Module -Name UniversalDashboard -Path $Staging -AcceptLicense -RequiredVersion 2.6 
-Copy-Item -Path "$Staging/UniversalDashboard/2.6/*" -Destination $Staging -Container
-Remove-Item -Path "$Staging/UniversalDashboard/2.6" -Force
+Copy-Item -Path "$Staging/UniversalDashboard/2.6.0/*" -Destination $Staging -Container
+Remove-Item -Path "$Staging/UniversalDashboard/2.6.0" -Force
