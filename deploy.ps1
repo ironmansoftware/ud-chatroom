0801583cf01f1a27ec8d122237328d3a063868b0
Adam Driscoll <adam.r.driscoll@gmail.com>
2019-09-15 13:48:15 +0530

    Install code editor

--- deploy.ps1
+++ deploy.ps1
@@ -4,5 +4,10 @@ Copy-Item ./ud-chatroom.psd1 $Staging
 Copy-Item ./ud-chatroom.psm1 $Staging
 Copy-Item ./dashboard.ps1 $Staging
 Save-Module -Name UniversalDashboard -Path $Staging -AcceptLicense -RequiredVersion 2.6 
+Save-Module -Name UniversalDashboard.CodeEditor -Path $Staging -RequiredVersion 1.0
 Copy-Item -Path "$Staging/UniversalDashboard/2.6.0/*" -Destination $Staging -Container -Recurse
 Remove-Item -Path "$Staging/UniversalDashboard/2.6.0" -Force -Recurse 
+
+$CodeEditorPath = New-Item -ItemType Directory -Path "$Staging/UniversalDashboard.CodeEdtior"
+Copy-Item -Path "$Staging/UniversalDashboard.CodeEditor/1.0.0/*" -Destination $CodeEditorPath.FullName -Container -Recurse
+Remove-Item -Path "$Staging/UniversalDashboard.CodeEditor/1.0.0" -Force -Recurse 
